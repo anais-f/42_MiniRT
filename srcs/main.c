9bf7d8ea2d67b0023906f9da86677027c5697445
Adri-cncl <cancel.adrien@gmail.com>
2024-11-28 12:29:10 +0100

    WIP main.c

--- srcs/main.c
+++ srcs/main.c
@@ -13,22 +13,17 @@ int main(int argc, char **argv)
 	//init minirt -> voir donnees cam et angle
 	init_camera(&minirt); // for test pars
 //	print_parsing(&minirt);
-
 	if (mlx_init_protected(&minirt.img) != 0)
 	{
 		mlx_destroy_all(&minirt.img);
 		array_free(&minirt.objects);
 		return (-1);
 	}
-
 	/************************* */
-
 	render_scene(&minirt, &minirt.img);
-
-
 	/**************************** */
 	mlx_put_image_to_window(minirt.img.mlx_ptr, minirt.img.win_ptr, minirt.img.img, 0, 0); //  a supprimer apres car compris dans loop
-	mlx_launch_event_and_loop(&minirt.img); // ici le put image  	
+	mlx_launch_event_and_loop(&minirt.img); // ici le put image
 	mlx_destroy_all(&minirt.img);
 	array_free(&minirt.objects);
 	return (0);
