9e33d81ff4a6be608c32d92f21cda2152bca6439
anfichet <anfichet@student.42lyon.fr>
2024-12-04 13:10:56 +0100

    fix create_ray with fat FOV

--- srcs/init.c
+++ srcs/init.c
@@ -4,7 +4,7 @@ int	init_minirt(t_minirt *minirt)
 {
 	minirt->cam.world_dir = (t_vec3){0, 0, 1};
 	minirt->cam.FOV *= M_PI / 180;
-	minirt->cam.aspect_ratio = (float)WIDTH_WIN / (float)HEIGHT_WIN;
+	minirt->cam.aspect_ratio = (float)HEIGHT_WIN / (float)WIDTH_WIN;
 	minirt->cam.fov_scale = tan(minirt->cam.FOV * 0.5);
 	minirt->to_radian = M_PI / 180;
 	minirt->to_degree = 180 / M_PI;
--- srcs/render/ray.c
+++ srcs/render/ray.c
@@ -5,15 +5,12 @@
 t_ray	create_ray_from_cam(t_minirt *minirt, int x, int y)
 {
 	t_ray	ray;
-	float	rh;
-	float	rv;
+	t_vec2	coord;
 
-	rh = 2 * tan(minirt->cam.FOV * 0.5) / WIDTH_WIN;
-	rv = 2 * tan(minirt->cam.FOV * HEIGHT_WIN / (WIDTH_WIN * 2)) / HEIGHT_WIN;
+	coord.x = (2 * (float)x / (float)WIDTH_WIN - 1)  * minirt->cam.fov_scale;
+	coord.y = (1 - 2 * (float)y / (float)HEIGHT_WIN) * minirt->cam.fov_scale * minirt->cam.aspect_ratio;
 	ray.origin = minirt->cam.position;
-	ray.direction.x = ((float)x - (float)WIDTH_WIN * 0.5) * rh;
-	ray.direction.y = ((float)HEIGHT_WIN * 0.5 - (float)y) * rv;
-	ray.direction.z = 1;
+	ray.direction = (t_vec3){coord.x, coord.y, 1};
 	ray.direction = normalize_vec3(ray.direction);
 	return (ray);
 }
