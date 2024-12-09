62eef025f436e267bf54ebb1890c1399666cb2ce
anfichet <anfichet@student.42lyon.fr>
2024-12-09 13:56:43 +0100

    abandon

--- srcs/display_mlx/event_mlx.c
+++ srcs/display_mlx/event_mlx.c
@@ -18,50 +18,31 @@ t_vec3 rotate_vec3(t_vec3 v, t_vec3 axis, float angle)
 	float	dot = dot_vec3(axis, v);
 	t_vec3	cross = cross_vec3(axis, v);
 
-	// if (norm_vec3(cross) < 1e-6) // Cas particulier : colinéaire ou antiparallèle
-    // {
-    //     float dot = dot_vec3(v, axis);
-    //     if (dot > 0) // Parallèle
-    //     {
-    //         return v; // Le vecteur reste inchangé
-    //     }
-    //     else // Antiparallèle
-    //     {
-    //         // Le vecteur doit être inversé selon l'angle
-    //         rotated_v.x = -v.x;
-    //         rotated_v.y = -v.y;
-    //         rotated_v.z = -v.z;
-    //         return rotated_v;
-    //     }
-    // }
-
 
 	rotated_v.x = v.x * cos_angle + cross.x * sin_angle + axis.x * dot * (1 - cos_angle);
 	rotated_v.y = v.y * cos_angle + cross.y * sin_angle + axis.y * dot * (1 - cos_angle);
 	rotated_v.z = v.z * cos_angle + cross.z * sin_angle + axis.z * dot * (1 - cos_angle);
-	rotated_v = normalize_vec3(rotated_v);
+
+	// rotated_v = normalize_vec3(rotated_v);
 	return rotated_v;
 }
+/* permet de mettre a jour le vecteur de direction de la camera
 
-// t_vec3 project_on_horizontal(t_vec3 v) {
-//     return (t_vec3){v.x, 0, v.z};
-// }
+ v le vecteur initial a faire tourner
+axis : l'axe autour duquel la rotation doit etre effectuee, il doit normalise
+angle : l'angle de rotation en radian
+rotated v : le vecteur v aores rotation
+
+les 3 rotated_v : utilisation de la formule de Rodrigues pour calculer le vecteur tourne
+
+*/
+
 
 t_mat	rotate_camera(t_minirt *mini, t_vec3 axis, float angle)
 {
+
 	float	theta;
-	// axis = (t_vec3){0, 1, 0};
 
-    // float original_y = mini->cam.direction.y; // Conserver la composante Y
-    // t_vec3 horizontal_dir = project_on_horizontal(mini->cam.direction);
-    // horizontal_dir = normalize_vec3(horizontal_dir);
-
-    // // Effectuer la rotation
-    // t_vec3 rotated_dir = rotate_vec3(horizontal_dir, axis, angle);
-
-    // // Réintroduire la composante Y d'origine
-    // rotated_dir.y = original_y;
-    // mini->cam.direction = normalize_vec3(rotated_dir);
 
 	mini->cam.direction = normalize_vec3(rotate_vec3(mini->cam.direction, axis, angle));
 	theta = theta_calc(mini->cam.world_dir, mini->cam.direction);
@@ -74,12 +55,20 @@ t_mat	rotate_camera(t_minirt *mini, t_vec3 axis, float angle)
 	return(rodrigues_rot(mini, normal_cam, theta));
 }
 
+// t_mat rotate_camera(t_minirt *mini, t_vec3 axis, float angle)
+// {
+//     // Applique la rotation directement à la direction de la caméra
+//     mini->cam.direction = rotate_vec3(mini->cam.direction, axis, angle);
+//     mini->cam.direction = normalize_vec3(mini->cam.direction);
+//     return matrix_identity(); // Retourne la matrice d'identité, car on ne la modifie pas ici
+// }
+
 
 int	kb_event(int key, t_minirt *mini)
 {
 	float move_dst = 0.3;
     t_vec3 up ;
-	//up = (t_vec3){0, 1, 0};
+	up = (t_vec3){0, 1, 0};
 	up = mult_vec3_matrix((t_vec3){0, 1, 0}, mini->cam.rot_mat);
 	up = normalize_vec3(up);
 	//printf("up = %f %f %f\n", up.x, up.y, up.z);
@@ -105,7 +94,6 @@ int	kb_event(int key, t_minirt *mini)
 		mini->cam.rot_mat = rotate_camera(mini, up, -angle);
 	else if (key == E_KB)
 		mini->cam.rot_mat = rotate_camera(mini, up, angle);
-
 
 
 	render_scene(mini, &mini->img);
